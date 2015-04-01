1c01156674685c15f985999c85fda5843fddf5d5
Bernat Borrás Paronella <bernatbor15@gmail.com>
2015-04-01 14:14:09 +0200

    Add second app

---
+++ secondmodule/src/main/java/com/alorma/secondmodule/LoginActivity.java
@@ -0,0 +1,59 @@
+package com.alorma.secondmodule;
+
+import android.accounts.Account;
+import android.accounts.AccountAuthenticatorActivity;
+import android.accounts.AccountManager;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.EditText;
+
+/**
+ * Created by Bernat on 01/04/2015.
+ */
+public class LoginActivity extends AccountAuthenticatorActivity {
+    public static final String ADDING_FROM_ACCOUNTS = "ADDING_FROM_ACCOUNTS";
+    public static final String ARG_ACCOUNT_TYPE = "ARG_ACCOUNT_TYPE";
+    public static final String ARG_AUTH_TYPE = "ARG_AUTH_TYPE";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle(getString(R.string.app_name));
+
+        final EditText editText = (EditText) findViewById(R.id.editText);
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String text = editText.getText().toString();
+                if (!TextUtils.isEmpty(text)) {
+                    addAccount(text);
+                }
+            }
+        });
+    }
+
+    private void addAccount(String text) {
+        AccountManager accountManager = AccountManager.get(this);
+
+        Account account = new Account(text, getString(R.string.account_type));
+        accountManager.addAccountExplicitly(account, null, null);
+
+        Bundle result = new Bundle();
+        result.putString(AccountManager.KEY_ACCOUNT_NAME, account.name);
+        result.putString(AccountManager.KEY_ACCOUNT_TYPE, account.type);
+
+        setAccountAuthenticatorResult(result);
+
+        Intent intent = new Intent();
+        intent.putExtras(result);
+        setResult(RESULT_OK, intent);
+        finish();
+    }
+}
--- secondmodule/src/main/java/com/alorma/secondmodule/MainActivity.java
+++ secondmodule/src/main/java/com/alorma/secondmodule/MainActivity.java
@@ -1,38 +1,82 @@
 package com.alorma.secondmodule;
 
+import android.accounts.Account;
+import android.accounts.AccountManager;
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
-
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class MainActivity extends ActionBarActivity {
+
+    private static final int RETURN_CODE = 242;
+    private ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivityForResult(intent, RETURN_CODE);
+            }
+        });
+
+        listView = (ListView) findViewById(R.id.list);
+
+        showAccounts();
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    private void showAccounts() {
+        Account[] accounts = getAccounts();
+
+        AccountsAdapter accountsAdapter = new AccountsAdapter(this, accounts);
+
+        listView.setAdapter(accountsAdapter);
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    public Account[] getAccounts() {
+        return AccountManager.get(this).getAccountsByType(getString(R.string.account_type));
+    }
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    private class AccountsAdapter extends ArrayAdapter<Account> {
+
+        private final LayoutInflater mInflater;
+
+        public AccountsAdapter(Context context, Account[] objects) {
+            super(context, 0, objects);
+            this.mInflater = LayoutInflater.from(context);
         }
 
-        return super.onOptionsItemSelected(item);
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            View view = this.mInflater.inflate(android.R.layout.simple_list_item_1, parent, false);
+
+            TextView textView = (TextView) view.findViewById(android.R.id.text1);
+
+            textView.setText(getItem(position).name);
+
+            return view;
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_OK && requestCode == RETURN_CODE) {
+          showAccounts();
+        }
     }
 }
